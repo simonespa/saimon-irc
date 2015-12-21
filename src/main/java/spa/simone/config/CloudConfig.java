3ada5934462d69ee3ec3f6f6052a5dbac454aed1
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2015-12-21 15:48:53 +0000

    Initial commit.

--- src/main/java/spa/simone/config/CloudConfig.java
+++ src/main/java/spa/simone/config/CloudConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Profile("external")
-@PropertySource("file:/home/developer/external.properties")
-public class ExternalConfig {
+@Profile("cloud")
+@PropertySource("classpath:cloud.properties")
+public class CloudConfig {
 }
